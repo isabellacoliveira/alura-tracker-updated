389824e20844b3354e846e832317f5444b5d93d9
Vinicios Neves <vinny@mac.local>
2021-09-07 13:52:55 -0300

    fim video 3.1

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,12 @@
     </div>
     <div class="column is-three-quarter">
       <Formulario />
-      <!-- Lista de Tarefas ???? -->
+      <div class="lista">
+        <Tarefa />
+        <Tarefa />
+        <Tarefa />
+        <Tarefa />
+      </div>
     </div>
   </main>
 </template>
@@ -14,16 +19,20 @@
 import { defineComponent } from 'vue';
 import BarraLateral from './components/BarraLateral.vue'
 import Formulario from './components/Formulario.vue'
+import Tarefa from './components/Tarefa.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
     BarraLateral,
-    Formulario
+    Formulario,
+    Tarefa
   }
 });
 </script>
 
 <style>
-
+.lista {
+  padding: 1.25rem;
+}
 </style>
