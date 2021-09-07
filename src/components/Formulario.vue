f3ff64c726680d5302bf4d231d84c6433054adfe
Vinicios Neves <vinny@mac.local>
2021-09-07 13:27:52 -0300

    fim video 2.3

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -10,10 +10,11 @@
           type="text"
           class="input"
           placeholder="Qual tarefa você deseja iniciar?"
+          v-model="descricao"
         />
       </div>
       <div class="column">
-        <Temporizador />
+        <Temporizador @aoTemporizadorFinalizado="finalizarTarefa"/>
       </div>
     </div>
   </div>
@@ -27,6 +28,18 @@ export default defineComponent({
   name: "Formulário",
   components: {
     Temporizador
+  },
+  data () {
+    return {
+      descricao: ''
+    }
+  },
+  methods: {
+    finalizarTarefa (tempoDecorrido: number) : void {
+      console.log('tempo da tarefa', tempoDecorrido)
+      console.log('descrição da tarefa', this.descricao)
+      this.descricao = ''
+    }
   }
 });
 </script>
--- src/components/Temporizador.vue
+++ src/components/Temporizador.vue
@@ -22,6 +22,7 @@ import Cronometro from './Cronometro.vue'
 
 export default defineComponent({
   name: "Temporizador",
+  emits: ['aoTemporizadorFinalizado'],
   components: {
     Cronometro
   },
@@ -44,6 +45,8 @@ export default defineComponent({
     finalizar () {
       this.cronometroRodando = false
       clearInterval(this.cronometro)
+      this.$emit('aoTemporizadorFinalizado', this.tempoEmSegundos)
+      this.tempoEmSegundos = 0
     }
   }
 });
